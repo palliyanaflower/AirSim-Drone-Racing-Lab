808810f276e9d55d31068fdf65902e2286a2ceed
kalliyan lay <kalliyanlay@gmail.com>
2026-03-07 22:41:28 -0700

    docker container working. can see simulator

--- docker/run_docker_image.sh
+++ docker/run_docker_image.sh
@@ -1,68 +1,53 @@
-# !bin/bash
-
+#!/bin/bash
 # **Usage**
-# - for running default image, training binaries, in windowed mode:    
-#     `$ ./run_docker_image.sh "" training` 
-# - for running default image, training binaries, in headless mode:    
+# - for running default image, training binaries, in windowed mode:
+#     `$ ./run_docker_image.sh "" training`
+# - for running default image, training binaries, in headless mode:
 #     `$ ./run_docker_image.sh "" training headless`
-# - for running a custom image in windowed mode, pass in you image name and tag:    
+# - for running a custom image in windowed mode, pass in your image name and tag:
 #     `$ ./run_docker_image.sh DOCKER_IMAGE_NAME:TAG`
-# - for running a custom image in headless mode, pass in you image name and tag, followed by "headless":    
-#      `$ ./run_docker_image.sh DOCKER_IMAGE_NAME:TAG headless`
+# - for running a custom image in headless mode, pass in your image name and tag, followed by "headless":
+#     `$ ./run_docker_image.sh DOCKER_IMAGE_NAME:TAG headless`
+# Example: ./run_docker_image.sh airsim_neurips:12.3.1-devel-ubuntu22.04
 
-# This script takes two optional arguments
+DOCKER_IMAGE_NAME=${1:-airsim_neurips:12.3.1-devel-ubuntu22.04}
+IS_HEADLESS=${3:-notheadless}
 
-# 1st argument is the name (and tag) of the dockerfile to run
-# by default, it is set to "adrl:10.0-devel-ubuntu18.04"
-# else user can specify a docker image as follows:
-# $ ./run_docker_image.sh DOCKER_IMAGE_NAME:TAG
-DOCKER_IMAGE_NAME=${1:-adrl:10.0-devel-ubuntu18.04}
+# Allow all local X server connections
+xhost + > /dev/null 2>&1
 
-# 2nd argument: if user passes "headless", binary runs in headless mode
-IS_HEADLESS=${2:-notheadless}
-
-# this block is for running X apps in docker
+# Setup xauth for X11 forwarding
 XAUTH=/tmp/.docker.xauth
-if [[ ! -f $XAUTH ]]
-then
-    xauth_list=$(xauth nlist :0 | sed -e 's/^..../ffff/')
-    if [ ! -z "$xauth_list" ]
-    then
-        echo $xauth_list | xauth -f $XAUTH nmerge -
-    else
-        touch $XAUTH
-    fi
-    chmod a+r $XAUTH
+touch $XAUTH
+chmod a+r $XAUTH
+if [[ -n "$DISPLAY" ]]; then
+    xauth nlist "$DISPLAY" | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge - 2>/dev/null
 fi
 
-# per use the following commented out code for different options
-UNREAL_BINARY_COMMAND="bash /home/airsim_user/ADRL/ADRL.sh -windowed -opengl"
+UNREAL_BINARY_COMMAND="sudo ldconfig && bash /home/airsim_user/ADRL/ADRL/ADRL.sh -windowed -vulkan -nosound"
 
-# eleminate terminal output and run airsim process in the background
-# UNREAL_BINARY_COMMAND="bash /home/airsim_user/ADRL/ADRL.sh -windowed -opengl &>/dev/null &"
-
-# set window resolution
-# UNREAL_BINARY_COMMAND="/home/airsim_user/ADRL/ADRL.sh -windowed -ResX=1080 -ResY=720"
-
-# now, let's check if we need to run in headless mode or not
-# set SDL_VIDEODRIVER_VALUE to '' if windowed mode, 'offscreen' if headless mode
-SDL_VIDEODRIVER_VALUE='';
-if [[ $2 = "headless" ]]; then 
-    SDL_VIDEODRIVER_VALUE='offscreen';
+SDL_VIDEODRIVER_VALUE=''
+if [[ $2 = "headless" ]]; then
+    SDL_VIDEODRIVER_VALUE='offscreen'
 fi
 
-# now, set the environment varible SDL_VIDEODRIVER to SDL_VIDEODRIVER_VALUE
-# and tell the docker container to execute UNREAL_BINARY_COMMAND
-nvidia-docker run -it \
-    -e SDL_VIDEODRIVER=$SDL_VIDEODRIVER_VALUE \
-    -e SDL_HINT_CUDA_DEVICE='0' \
+docker run -it \
+    --gpus all \
+    --ipc=host \
+    --privileged \
     --net=host \
-    --env="DISPLAY=$DISPLAY" \
-    --env="QT_X11_NO_MITSHM=1" \
-    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-    -env="XAUTHORITY=$XAUTH" \
-    --volume="$XAUTH:$XAUTH" \
-    --runtime=nvidia \
+    -e NVIDIA_DRIVER_CAPABILITIES=all \
+    -e NVIDIA_VISIBLE_DEVICES=all \
+    -e DISPLAY=$DISPLAY \
+    -e XDG_RUNTIME_DIR=/tmp \
+    -e SDL_VIDEODRIVER=$SDL_VIDEODRIVER_VALUE \
+    -e SDL_HINT_CUDA_DEVICE=0 \
+    -e QT_X11_NO_MITSHM=1 \
+    -e XAUTHORITY=$XAUTH \
+    -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+    -v $XAUTH:$XAUTH:rw \
+    -v /usr/lib/x86_64-linux-gnu:/usr/lib/x86_64-linux-gnu:ro \
+    -v /usr/share/vulkan/icd.d/nvidia_icd.json:/usr/share/vulkan/icd.d/nvidia_icd.json:ro \
     --rm \
     $DOCKER_IMAGE_NAME \
     /bin/bash -c "$UNREAL_BINARY_COMMAND"
